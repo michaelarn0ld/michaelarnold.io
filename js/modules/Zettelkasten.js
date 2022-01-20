b0939124eb48454c07fe8261fc6b68be6ee85d4e
Michael Arnold <me@michaelarnold.io>
2022-01-20 00:47:11 -0600

    making tag graph for html docs

--- js/modules/Zettelkasten.js
+++ js/modules/Zettelkasten.js
@@ -1,19 +1,14 @@
 export const Zettelkasten = () => {
-    const apiUrl = (id) => `https://gitlab.com/api/v4/projects/29800188/repository/files/${id}%2fREADME.md?ref=main`
-
-    const fetchZettel = async (id) => {
-        const response = await fetch(apiUrl(id))
-        return response.json()
-    }
-
-    fetchZettel("202201191736")
-        .then(data => {
-            console.log(atob(data.content))
-        })
-
-    /*
-     * TODO
-     * - parse decoded data to HTML
-     * - somehow render that to the page?
-     * */
+    const zettels = Array.from(document.getElementById("zettels").children)
+    const tags = {}
+    // create a graph of tags
+    zettels.forEach(z => {
+        for (let tag of z.attributes) {
+            if (tag.name !== "id" && tag.name != "class") {
+                tags[tag.name] = tags[tag.name] === undefined ? [z] : [...tags[tag.name], z]
+            }
+        }
+    })
+    console.log(tags)
+    console.log(Object.keys(tags))
 }
