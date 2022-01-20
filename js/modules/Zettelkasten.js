3d8b3e0c36677c13d0c3102bc8093e9620ced422
Michael Arnold <me@michaelarnold.io>
2022-01-20 11:03:45 -0600

    Set implementation for Zettel tags: Next step -> rendering elements using matching set attributes

--- js/modules/Zettelkasten.js
+++ js/modules/Zettelkasten.js
@@ -9,6 +9,33 @@ export const Zettelkasten = () => {
             }
         }
     })
+
     console.log(tags)
-    console.log(Object.keys(tags))
+    const activeTags = new Set()
+
+    const toggleActiveTag = (tag) => {
+        if (activeTags.has(tag)) {
+            activeTags.delete(tag)
+        } else {
+            activeTags.add(tag)
+        }
+        console.log(activeTags)
+    }
+
+    const makeTag = (tag) => {
+        let element = document.createElement("div");
+        element.innerText = tag
+        element.onclick = () => toggleActiveTag(tag)
+        return element
+    }
+
+    const tagsElement = document.getElementById("tags")
+    Object.keys(tags).forEach(t => tagsElement.appendChild(makeTag(t)))
+
+    /*
+     *TODO
+     - If activeTags is empty then render everything
+     - If activeTags contains stuff then only render elements that
+       have matching attributes
+     * */
 }
