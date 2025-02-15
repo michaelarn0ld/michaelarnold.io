cb5d37c961e0539235c04c84d843e39233f0b2f5
Michael Arnold <me@michaelarnold.io>
2025-02-14 20:50:01 -0800

    basic infra is setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit, you fuggin druggo</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import biglez from '$lib/assets/biglez.jpg';
+</script>
+<h1>This shit is currently under construction</h1>
+<p>static app built on svelte, s3 + cloudfront, and github actions</p>
+<img alt="The project logo" src={biglez} />
